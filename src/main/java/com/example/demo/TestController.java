f905aa5eb8ce57b01e2c637db3f5aceec4e4bb4e
yangrui <201250028@smail.nju.edu.cn>
2024-03-13 21:18:23 +0800

    push

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    //test test test
+
 
     @RequestMapping("hello")
     public String hello() {
